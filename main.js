58e5052890d2bb8cca70a8e8e1bd6497aadf7e9b
Satorogojoo <marcra1428@gmail.com>
2024-09-03 21:16:23 -0400

    Add files via upload
    prueba

---
+++ main.js
@@ -0,0 +1,20 @@
+main.js
+// declarar la variable
+const nu1 = prompt("Ingresa el numero 1")
+const nu2 = prompt("Ingresa el primer 2")
+const nu3 = prompt("Ingresa el primer 3")
+
+
+// convertir a enterp
+const nu1Int = Number(nu1)
+const nu2Int = Number(nu2)
+const nu3Int = Number(nu3)
+
+// sumar los numeros
+const resultado = nu1Int + nu2Int + nu3Int
+
+// calcular promedio
+const promedio = resultado / 3
+
+// mostrar el resultado
+alert(* Promedio de los tres numeros es: * + promedio)
